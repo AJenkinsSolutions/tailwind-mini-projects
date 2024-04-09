9a07857a78818755d5a23202c6d58601ee052b62
Alex Jenkins <ajenkinsuiw@gmail.com>
2024-04-09 10:10:06 -0500

    Add custom colors, custom screen sizes, google fonts

--- website-projects/my-web-projects/my-clipboard/tailwind.config.js
+++ website-projects/my-web-projects/my-clipboard/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      //CustomColors using hsl
+      colors: {
+        strongCyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(223, 100%, 69%)',
+        darkGrayishBlue: 'hsl(210, 10%, 33%)',
+        darkGrayishBlue: 'hsl(201, 11%, 66%)',
+      },
+      fontFamily: {
+        sans: ['Bai Jamjuree', 'sans-serif'], 
+      }
+    },
   },
   plugins: [],
 }
